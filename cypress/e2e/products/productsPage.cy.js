f55faf31c4a48a47f077c0b1745c57213d6624bc
Sebastian Gonzalez <seba14079783@gmail.com>
2025-08-20 14:32:17 -0400

    Avance test productos

--- cypress/e2e/products/productsPage.cy.js
+++ cypress/e2e/products/productsPage.cy.js
@@ -3,22 +3,37 @@ import commands from '../../support/commands';
 import data from '../../fixtures/data.json';
 
 {
-
     describe('Products Page Tests', () => {
         beforeEach(function() {
             cy.visit(data.urls.login);  
+            cy.login(data.users.standard.username, data.users.password);
         })  
         it('View peoduct list', () => {
-            cy.login(data.users.standard.username, data.users.password);
             cy.shouldBeVisible(selectors.products.inventoryContainer);
-            cy.getCount(selectors.products.inventoryList, selectors.products.inventoryItem)
-            .then((count) => {
-                expect(count).to.be.greaterThan(0);
-                let index = 1;
-                for (let i = 0; i < count; i++) {
-                    cy.log(`Accediendo al Item ${i + 1}`);
-                }
+            cy.get(selectors.products.inventoryItem).each(($item, index) => {
+                cy.wrap($item).within(() => {
+                    cy.shouldBeVisible(selectors.products.itemImg);
+                    cy.shouldBeVisible(selectors.products.itemName);
+                    cy.shouldBeVisible(selectors.products.itemPrice);
+                    cy.shouldBeVisible(selectors.products.itemButton);   
+                });
+                cy.log(`Producto ${index + 1} validado correctamente ✅`);
             });
+        });
+        it('filter and sort products', () => {
+            cy.get('.product_sort_container').should('be.visible')
+            // Validar que la lista de productos esté ordenada descendentemente (Z-A)
+            cy.get('.product_sort_container').select('za');
+            cy.validateOrder(selectors.products.inventoryItem, 'desc', 'text');
+            // Validar que la lista de productos esté ordenada por defecto (A-Z)
+            cy.get('.product_sort_container').select('az');
+            cy.validateOrder(selectors.products.inventoryItem, 'asc', 'text');
+            // Validar que la lista de productos esté ordenada por precio menor a mayor
+            cy.get('.product_sort_container').select('lohi');
+            cy.validateOrder(selectors.products.inventoryItem, 'asc', 'number');
+            // Validar que la lista de productos esté ordenada por precio mayor a menor
+            cy.get('.product_sort_container').select('hilo');
+            cy.validateOrder(selectors.products.inventoryItem, 'desc', 'number');
         });
     })
 }
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,12 +2,40 @@ import selectors from '../fixtures/selectors.json';
 import './wrapper'
 
 Cypress.Commands.add('login', (username, password) => {
-        cy.waitSeconds(2);
+        cy.waitSeconds(1);
         cy.get(selectors.login.username)
         cy.writeIn(selectors.login.username, username);
-        cy.waitSeconds(2);
+        cy.waitSeconds(1);
         cy.get(selectors.login.password)
         cy.writeIn(selectors.login.password, password);
-        cy.waitSeconds(2);
+        cy.waitSeconds(1);
         cy.clickIn(selectors.login.loginButton);
+});
+
+/**
+ * Valida que una lista esté ordenada correctamente
+ * @param {'asc'|'desc'} direction - Dirección de orden (ascendente/descendente)(a-z, menor-mayor / z-a, mayor-menor)
+ * @param {'text'|'number'} type - Tipo de dato a evaluar (text: nombre de producto, number: precio)
+ */
+Cypress.Commands.add('validateOrder', (selector, direction, type) => {
+  cy.get(selector).then(($els) => {
+    let values = [...$els].map(el => {
+      if (type === 'number') {
+        return parseFloat(el.innerText.replace('$', ''));
+      }
+      return el.innerText.trim();
+    });
+
+    let sorted = [...values].sort((a, b) => {
+      return type === 'number'
+        ? a - b
+        : a.localeCompare(b);
+    });
+
+    if (direction === 'desc') {
+      sorted.reverse();
+    }
+
+    expect(values).to.deep.equal(sorted);
+  });
 });
